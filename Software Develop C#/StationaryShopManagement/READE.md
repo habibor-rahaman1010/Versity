836b9e9e3e1ba93c12b0b8ba4c5cae3c13b38d58
habibor-rahaman1010 <habibor144369@gmail.com>
2023-12-15 07:42:36 +0600

    ok

--- Software Develop C#/StationaryShopManagement/READE.md
+++
@@ -1,1 +0,0 @@
-project demo link:  https://docs.google.com/document/d/14EK9CPN-MbAcMbzAV5CGXppumnxZ7MOM-kSZBw6hZTU/edit?usp=sharing
--- Versity_Project/StationaryShopManagement/BO/Purchase.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StationaryShopManagement.BO
-{
-    public class Purchase : OperationOnProduct
-    {
-        public string VendorName { set; get; }
-        public double TotalAmount { set; get; }
-    }
-}
